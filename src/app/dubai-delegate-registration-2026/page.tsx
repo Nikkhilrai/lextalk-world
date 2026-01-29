95d712b0a24dd152cbdfefee8201b4c140159600
Nikhil Rai <nikkhhilrai@gmail.com>
2026-01-29 16:18:33 +0530

    Integrate WhyAttend component into Dubai registration page

--- src/app/dubai-delegate-registration-2026/page.tsx
+++ src/app/dubai-delegate-registration-2026/page.tsx
@@ -1,5 +1,11 @@
 import Hero from "./components/Hero";
+import WhyAttend from "./components/WhyAttend";
 
 export default function Page() {
-    return <Hero />;
+    return (
+        <main>
+            <Hero />
+            <WhyAttend />
+        </main>
+    );
 }
