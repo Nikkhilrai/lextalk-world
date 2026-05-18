e037b3cf75bd36978b682329dbd182f4c9664b10
Nikhil Rai <nikkhhilrai@gmail.com>
2026-05-18 14:28:31 +0530

    Fix bio modal z-index to render above navbar on Bangalore speakers page

--- src/app/bangalore-2026/bangalore-speakers-list.tsx
+++ src/app/bangalore-2026/bangalore-speakers-list.tsx
@@ -377,7 +377,7 @@ export default function BangaloreSpeakersList() {
             {/* Biography Modal */}
             <AnimatePresence>
                 {selectedSpeaker && (
-                    <div className="fixed inset-0 z-[100] flex items-center justify-center p-4">
+                    <div className="fixed inset-0 z-[999999] flex items-center justify-center p-4">
                         <motion.div
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
